5fe60ea734b3b4caca52f5ad3510d75707de809c
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-02-20 16:53:54 -0300

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        
+      <h1>Welcome to Robofriend</h1>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
